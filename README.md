7563440ff1ba550e45365189e46b77421f5fbfbb
Dan Brown <dan@familybrown.org>
2020-08-03 07:27:27 -0400

    Rewrite for Caddy v2 and modularity

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # freenas-iocage-heimdall
-Script to install [Heimdall Dashboard](https://heimdall.site/) in a FreeNAS iocage jail
+Script to create a FreeNAS jail and install [Heimdall Dashboard](https://heimdall.site/) in it
 
 # Installation
-Change to a convenient directory, clone the directory using `git clone https://github.com/danb35/freenas-iocage-heimdall`, change to the freenas-iocage-heimdall directory, and edit the variables at the top of the script.  Then run the script with `./heimdall-jail.sh`.
+Change to a convenient directory, clone the repository using `git clone https://github.com/danb35/freenas-iocage-heimdall`, change to the freenas-iocage-heimdall directory, and create a configuration file called `heimdall-config` with your favorite text editor (if you don't have a favorite text editor, `nano` is a good choice--run `nano heimdall-config`).  Then run the script with `script heimdall.log ./heimdall-jail.sh`.
+
+## Configuration options
+In its minimal form, the configuration file would look like this:
+```
+JAIL_IP="192.168.1.78"
+DEFAULT_GW_IP="192.168.1.1"
+POOL_PATH="/mnt/tank"
+```
+
+* JAIL_IP:  The IP address to assign the jail.  You may optionally specify a netmask in CIDR notion.  If none is specified, the default is /24.  Values of less than 8 bits or more than 30 bits will also result in a 24-bit netmask.
+* DEFAULT_GW_IP:  The IP address of your default gateway.
+* POOL_PATH:  The path to your main data pool (e.g., `/mnt/tank`).  The Caddyfile and Heimdall installation files (i.e., the web pages themselves) will be stored there, in $POOL_PATH/apps/heimdall.  If you have more than one pool, choose the one you want to use for this purpose.
+* FILE:  Optional.  The filename to download, which identifies the version of Heimdall to download.  Default is 2.2.2.  To check for a more recent release, see the [Heimdall release page](https://github.com/linuxserver/Heimdall/releases).
+* DNS_PLUGIN:  Optional.  If you want to compile Caddy using a DNS validation plugin, specify it here.  To see the available plugins, go to the [Caddy download page](https://caddyserver.com/download).  Only enter the actual name of the plugin--e.g., if the plugin is listed with a package name of `github.com/caddy-dns/cloudflare`, enter only `cloudflare`.  **Note:** This will not configure the installation to use this plugin--you'll still need to handle that in your Caddyfile.  It will, however, make the plugin available.
+* JAIL_NAME:  Optional.  The name of the jail.  If not given, will default to "heimdall".
+
+## Post-install configuration
+This script uses the [Caddy](https://caddyserver.com/) web server, which supports automatic HTTPS, reverse proxying, and many other powerful features.  It is configured using a Caddyfile, which is stored at `/usr/local/www/Caddyfile` in your jail, and under `/apps/heimdall/` on your main data pool.  You can edit it as desired to enable these or other features.  For further information, see [my Caddy script](https://github.com/danb35/freenas-iocage-caddy), specifically the included `Caddyfile.example`, or the [Caddy docs](https://caddyserver.com/docs/caddyfile).
 
 # Support
 Questions and discussion should be directed to https://forum.freenas-community.org/t/install-heimdall-dashboard-in-a-jail-script-freenas-11-2/35
--- heimdall-jail.sh
+++ heimdall-jail.sh
@@ -5,15 +5,62 @@
 
 # https://forum.freenas-community.org/t/install-heimdall-dashboard-in-a-jail-script-freenas-11-2/35
 
-# Set these variables as appropriate
-# FILE should be set to the version number of the latest release, with .tar.gz added
-JAIL_NAME="heimdall"
-JAIL_IP=192.168.1.204
-DEFAULT_GW_IP=192.168.1.1
-CERT_EMAIL="nobody@example.com"
-FILE="2.2.2.tar.gz"
+# Check for root privileges
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run with root privileges"
+   exit 1
+fi
 
-# Don't change anything below here
+#####
+#
+# General configuration
+#
+#####
+
+# Initialize defaults
+JAIL_NAME="heimdall"
+JAIL_IP=""
+DEFAULT_GW_IP=""
+POOL_PATH=""
+FILE="2.2.2.tar.gz"
+DNS_PLUGIN=""
+CONFIG_NAME="heimdall-config"
+
+# Check for heimdall-config and set configuration
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
+if ! [ -e "${SCRIPTPATH}"/"${CONFIG_NAME}" ]; then
+  echo "${SCRIPTPATH}/${CONFIG_NAME} must exist."
+  exit 1
+fi
+. "${SCRIPTPATH}"/"${CONFIG_NAME}"
+
+# Error checking and config sanity check
+if [ -z "${JAIL_IP}" ]; then
+  echo 'Configuration error: JAIL_IP must be set'
+  exit 1
+fi
+if [ -z "${DEFAULT_GW_IP}" ]; then
+  echo 'Configuration error: DEFAULT_GW_IP must be set'
+  exit 1
+fi
+if [ -z "${POOL_PATH}" ]; then
+  echo 'Configuration error: POOL_PATH must be set'
+  exit 1
+fi
+
+# Extract IP and netmask, sanity check netmask
+IP=$(echo ${JAIL_IP} | cut -f1 -d/)
+NETMASK=$(echo ${JAIL_IP} | cut -f2 -d/)
+if [ "${NETMASK}" = "${IP}" ]
+then
+  NETMASK="24"
+fi
+if [ "${NETMASK}" -lt 8 ] || [ "${NETMASK}" -gt 30 ]
+then
+  NETMASK="24"
+fi
+
 RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g" | sed "s/-p[0-9]*//")
 mountpoint=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
 
@@ -21,21 +68,51 @@ mountpoint=$(zfs get -H -o value mountpoint $(iocage get -p)/iocage)
 cat <<__EOF__ >/tmp/pkg.json
 {
   "pkgs":[
-  "nano", "caddy", "php72", "php72-mbstring", "php72-zip", "php72-tokenizer", 
-  "php72-openssl", "php72-pdo", "php72-pdo_sqlite", "php72-filter", "php72-xml", 
-  "php72-ctype", "php72-json", "sqlite3", "php72-session", "php72-hash"
+  "nano", "php73", "php73-mbstring", "php73-zip", "php73-tokenizer", 
+  "php73-openssl", "php73-pdo", "php73-pdo_sqlite", "php73-filter", "php73-xml", 
+  "php73-ctype", "php73-json", "sqlite3", "php73-session", "php73-hash",
+  "go", "git"
   ]
 }
 __EOF__
 
 if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" \
-  ip4_addr="vnet0|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" \
+  ip4_addr="vnet0|${IP}/${NETMASK}" defaultrouter="${DEFAULT_GW_IP}" boot="on" \
   host_hostname="${JAIL_NAME}" vnet="on"
 then
 	echo "Failed to create jail"
 	exit 1
 fi
 rm /tmp/pkg.json
+
+# Store Caddyfile and data outside the jail
+mkdir -p "${POOL_PATH}"/apps/heimdall
+iocage fstab -a "${JAIL_NAME}" "${POOL_PATH}"/apps/heimdall /usr/local/www nullfs rw 0 0
+
+# Build xcaddy, use it to build Caddy
+if ! iocage exec "${JAIL_NAME}" "go get -u github.com/caddyserver/xcaddy/cmd/xcaddy"
+then
+  echo "Failed to get xcaddy, terminating."
+  exit 1
+fi
+if ! iocage exec "${JAIL_NAME}" go build -o /usr/local/bin/xcaddy github.com/caddyserver/xcaddy/cmd/xcaddy
+then
+  echo "Failed to build xcaddy, terminating."
+  exit 1
+fi
+if [ -n "${DNS_PLUGIN}" ]; then
+  if ! iocage exec "${JAIL_NAME}" xcaddy build --output /usr/local/bin/caddy --with github.com/caddy-dns/"${DNS_PLUGIN}"
+  then
+    echo "Failed to build Caddy with ${DNS_PLUGIN} plugin, terminating."
+    exit 1
+  fi  
+else
+  if ! iocage exec "${JAIL_NAME}" xcaddy build --output /usr/local/bin/caddy
+  then
+    echo "Failed to build Caddy without plugin, terminating."
+    exit 1
+  fi  
+fi
 
 iocage exec "${JAIL_NAME}" mkdir -p /usr/local/www/html
 iocage exec "${JAIL_NAME}" fetch -o /tmp https://github.com/linuxserver/Heimdall/archive/"${FILE}"
@@ -47,23 +124,79 @@ iocage exec "${JAIL_NAME}" touch /usr/local/www/html/database/app.sqlite
 iocage exec "${JAIL_NAME}" chmod 664 /usr/local/www/html/database/app.sqlite
 iocage exec "${JAIL_NAME}" sysrc php_fpm_enable=YES
 iocage exec "${JAIL_NAME}" sysrc caddy_enable=YES
-iocage exec "${JAIL_NAME}" sysrc caddy_cert_email="${CERT_EMAIL}"
 
 # Create Caddyfile
 cat <<__EOF__ >"${mountpoint}"/jails/"${JAIL_NAME}"/root/usr/local/www/Caddyfile
 *:80 {
-gzip
-root /usr/local/www/html/public
-        fastcgi / 127.0.0.1:9000 php {
-                env PATH /bin
-        }
-rewrite {
-  r .*
-  ext / .config
-  to /index.php/{query}
-}
+	encode gzip
+
+	log {
+		output file /var/log/heimdall_access.log
+		format single_field common_log
+	}
+
+	root * /usr/local/www/html/public
+	file_server
+
+	php_fastcgi 127.0.0.1:9000
+
+	# Add reverse proxy directives here if desired
+
 }
 __EOF__
+
+# Create Caddy rc script
+cat <<__EOF__ >"${mountpoint}"/jails/"${JAIL_NAME}"/root/usr/local/etc/rc.d/caddy
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: caddy
+# REQUIRE: LOGIN DAEMON NETWORKING
+# KEYWORD: shutdown
+
+# Add the following lines to /etc/rc.conf.local or /etc/rc.conf
+# to enable this service:
+# caddy_enable (bool):   Set to NO by default. Set it to YES to enable caddy.
+#
+# caddy_config (string): Optional full path for caddy config file
+# caddy_adapter (string):  Optional adapter type if the configuration is not in caddyfile format
+# caddy_extra_flags (string):  Optional flags passed to caddy start
+# caddy_logfile (string):     Set to "/var/log/caddy.log" by default.
+#                             Defines where the process log file is written, this is not a web access log
+
+. /etc/rc.subr
+
+name=caddy
+rcvar=caddy_enable
+desc="Caddy 2 is a powerful, enterprise-ready, open source web server with automatic HTTPS written in Go"
+
+load_rc_config $name
+
+# Defaults
+: ${caddy_enable:=NO}
+: ${caddy_config:="/usr/local/etc/Caddyfile"}
+: ${caddy_adapter:=caddyfile}
+: ${caddy_extra_flags:=""}
+: ${caddy_logfile="/var/log/caddy.log"}
+
+command="/usr/local/bin/${name}"
+caddy_flags="--config ${caddy_config} --adapter ${caddy_adapter}"
+pidfile="/var/run/${name}.pid"
+
+required_files="${caddy_config} ${command}"
+
+# Extra Commands
+extra_commands="validate reload"
+start_cmd="${command} start ${caddy_flags} ${caddy_extra_flags} --pidfile ${pidfile} >> ${caddy_logfile} 2>&1"
+validate_cmd="${command} validate ${caddy_flags}"
+reload_cmd="${command} reload ${caddy_flags}"
+
+run_rc_command "$1"
+__EOF__
+
+iocage exec "${JAIL_NAME}" chmod +x /usr/local/etc/rc.d/caddy
 
 iocage exec "${JAIL_NAME}" cp /usr/local/www/html/.env.example /usr/local/www/html/.env
 iocage exec "${JAIL_NAME}" sh -c 'cd /usr/local/www/html/ && php artisan key:generate'
